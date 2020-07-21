940166a6ca8a4d095b4408966f9558a80e04e18c
Angels Of War <angiechangpagne@gmail.com>
2020-07-20 21:39:02 -0700

    auth middleware

--- server.js
+++ server.js
@@ -20,12 +20,28 @@ app.prepare().then(() => {
   server.use(session({ secure: true, sameSite: 'none' }, server));
 
   server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  server.use(
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['read_products'],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.redirect('/');
+      },
+    }),
+  );
+
+  server.use(verifyRequest());
+
   server.use(async(ctx) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
     return;
   });
+  
 });
 // var express = require('express');
 
