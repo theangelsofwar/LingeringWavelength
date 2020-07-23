4b0be8bfdf602163676d2c9eb5ef62db057a8a7b
Angels Of War <angiechangpagne@gmail.com>
2020-07-22 21:29:27 -0700

    morphe

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 require('isomorphic-fetch');
 const dotenv = require('dotenv');
-dotenv.config();
 const Koa = require('koa');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
-const { default: Server } = require('next/dist/next-server/server/next-server');
 
 
-const port = parseInt(process.env.PORT, 10 || 3000);
-const dev = process.send.NODE_ENV !== 'production';
+dotenv.config();
+
+//const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+//const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
+//const Router = require('koa-router');
+//const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
+//const getSubscriptionUrl = require('./server/getSubscriptionUrl');
+
+// const { default: Server } = require('next/dist/next-server/server/next-server');
+
+const port = parseInt(process.env.PORT, 10)|| 3000;
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
